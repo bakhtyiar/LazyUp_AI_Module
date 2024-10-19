65ca6d21cec9fee7fe340fe89689770addae52df
BakhtiyarKh <->
2024-10-19 20:11:28 +0300

    Added miscellaneous files. Updated mouse listener

--- listenDeviceInput.py
+++ listenDeviceInput.py
@@ -3,6 +3,25 @@ import sys
 import signal
 from datetime import datetime
 import keyboard  # Убедитесь, что эта библиотека установлена
+import mouse  # Убедитесь, что эта библиотека установлена
+import pprint
+
+# Mouse buttons map
+mouse_buttons_map = {
+    'left': 1,
+    'right': 2,
+    'middle': 3,
+    'wheel': 4,
+    'x': 5,
+    'x2': 6,
+    'up': 7,
+    'down': 8,
+    'double': 9,
+    'vertical': 10,
+    'horizontal': 11,
+    'deltaPlus': 12,
+    'deltaMinus': 13,
+}
 
 logs = []
 is_working_mode = True
@@ -24,6 +43,22 @@ def on_key_event(event):
         log_event(event.scan_code, is_working_mode)
     except:
         print("Pressed button out from processing range")
+        
+
+def on_mouse_event(event):
+    global is_working_mode
+    try:
+        if isinstance(event, mouse.ButtonEvent) and event.button in mouse_buttons_map:
+            print(event.button)
+            print(mouse_buttons_map[event.button])
+            log_event(mouse_buttons_map[event.button], is_working_mode)
+        elif isinstance(event, mouse.WheelEvent) and event.delta is not None:
+            if event.delta > 0:
+                log_event(mouse_buttons_map["deltaPlus"], is_working_mode)
+            else:
+                log_event(mouse_buttons_map["deltaMinus"], is_working_mode)
+    except:
+        print("Pressed button out from processing range")
 
 def signal_handler(sig, frame):
     print("Прерывание процесса. Сохранение данных и выход.")
@@ -35,5 +70,7 @@ if __name__ == "__main__":
 
     signal.signal(signal.SIGINT, signal_handler)
 
-    keyboard.hook(on_key_event)  # Слушаем события клавиатуры
-    keyboard.wait()  # Ожидаем завершения
+    # keyboard.hook(on_key_event)  # Слушаем события клавиатуры
+    mouse.hook(on_mouse_event)  # Слушаем события мыши
+    # keyboard.wait()  # Ожидаем завершения
+    mouse.wait()  # Ожидаем завершения
