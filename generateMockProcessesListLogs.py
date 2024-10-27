7ead117c11395b7800ab662d7d76ff34b8aca572
BakhtiyarKh <->
2024-10-27 20:38:19 +0300

    Added generateMockProcessesListLogs

--- generateMockProcessesListLogs.py
+++ generateMockProcessesListLogs.py
@@ -2,6 +2,7 @@ import json
 import random
 import os
 from datetime import datetime, timedelta
+import time
 
 # Основные процессы
 mandatory_processes = [
@@ -18,7 +19,7 @@ joy_processes = ['steam', 'discord', 'origin']
 work_processes = ['whatsapp', 'telegram', 'word', 'excel']
 
 # Функция для генерации данных
-def generate_data():
+def generate_data(currentDateTimeStamp):
     is_working_mode = random.choice([True, False])
 
     processes = mandatory_processes.copy()
@@ -38,11 +39,13 @@ def generate_data():
             if random.random() < 0.80:  # 80% вероятность
                 processes.append(proc)
 
-    timestamp = datetime.now().isoformat()
+    # timestamp = datetime.now().isoformat()
+
+    processes.reverse()
 
     return {
         "is_working_mode": is_working_mode,
-        "timestamp": timestamp,
+        "timestamp": currentDateTimeStamp.isoformat(),
         "processes": processes
     }
 
@@ -50,28 +53,26 @@ currentDateTimeStamp = datetime.now() - timedelta(hours=0, minutes=0, seconds=5)
 
 # Генерация и запись файлов
 for i in range(100):
-    data = generate_data()
+    data = generate_data(currentDateTimeStamp)
     file_name = currentDateTimeStamp.strftime("%Y-%m-%d_%H-%M-%S") + ".json"
     currentDateTimeStamp = currentDateTimeStamp - timedelta(hours=0, minutes=0, seconds=5)
     
     # Убедимся, что имена файлов уникальны
     while os.path.exists(file_name):
-        file_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".json"
+        currentDateTimeStamp = currentDateTimeStamp - timedelta(hours=0, minutes=0, seconds=5)
+        file_name = currentDateTimeStamp.strftime("%Y-%m-%d_%H-%M-%S") + ".json"
+        # file_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".json"
 
-    with open(file_name, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
+    logs_folder_name = "processes_logs"
+
+    # Формируем имя файла в папке "logs_folder"
+    logs_file_name = os.path.join(logs_folder_name, f"{file_name}.json")
+
+    # Убеждаемся, что папка существует
+    os.makedirs(logs_folder_name, exist_ok=True)
+
+    with open(logs_file_name, 'w') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
 
     # Задержка на секунду, чтобы избежать одинаковых временных меток
-    time.sleep(1)
-
-# Генерация и запись файлов
-for i in range(100):
-    # Вычисляем название файла
-    adjusted_timestamp = current_timestamp - (5 * i)
-    file_name = datetime.datetime.fromtimestamp(adjusted_timestamp).strftime('%Y-%m-%d_%H-%M-%S') + '.json'
-    
-    # Создаем файл с указанным именем
-    with open(file_name, 'w') as f:
-        f.write(f'Файл создан с временной меткой: {adjusted_timestamp}\n')
-
-print(f'Создано {num_files} файлов.')
+    # time.sleep(1)
