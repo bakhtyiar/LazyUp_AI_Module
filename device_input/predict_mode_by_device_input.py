70a2e312b4676aba2c7c1699a9850b28028b3a16
BakhtiyarKh <->
2025-03-21 01:48:06 +0300

    Updated predicting for device_inputs. Applied updated predicting to process.py

--- device_input/predict_mode_by_device_input.py
+++ device_input/predict_mode_by_device_input.py
@@ -8,35 +8,47 @@ module_dir = os.path.dirname(os.path.abspath(__file__))
 directory_path = module_dir + './device_input_logs'  # Путь к директории с JSON-файлами
 model_path = module_dir + './predict_device_input.h5'  # Путь к модели
 
-# Считываем все логи
-all_logs = []
-for filename in os.listdir(directory_path):
-    if filename.endswith('.json'):
-        file_path = os.path.join(directory_path, filename)
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-            all_logs.extend(data['deviceLogs'])
+def predict_by_device_input(df: pd.DataFrame):
+    # Преобразуем timestamp в числовой формат
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df['timestamp_seconds'] = df['timestamp'].astype('int64') // 10**9  # Конвертация в секунды
+    # Сортировка по timestamp
+    df = df.sort_values('timestamp')
 
-# Подготовка данных
-df = pd.DataFrame(all_logs)
+    # Выбираем необходимые столбцы
+    df = df[['timestamp_seconds', 'buttonKey', 'isWorkingMode']]
+    prepared_data = df
 
-# Преобразуем timestamp в числовой формат
-df['timestamp'] = pd.to_datetime(df['timestamp'])
-df['timestamp_seconds'] = df['timestamp'].astype('int64') // 10**9  # Конвертация в секунды
-
-# Выбираем необходимые столбцы
-df = df[['timestamp_seconds', 'buttonKey', 'isWorkingMode']]
-prepared_data = df
-
-model = None
-try:
     model = joblib.load(model_path)
-except OSError:
-    print("Saved model not found")
 
-X = prepared_data[['timestamp_seconds', 'buttonKey']]
-y = prepared_data['isWorkingMode']
+    X = prepared_data[['timestamp_seconds', 'buttonKey']]
+    y = prepared_data['isWorkingMode']
 
-# Оценка модели
-predictions = model.predict(X)
+    # Оценка модели
+    ret = model.predict(X)
+    return ret
 
+def load_dataframe_device_input(amount_of_records=128):
+    # Считываем все логи
+    all_logs = []
+    for filename in os.listdir(directory_path):
+        if amount_of_records < 1:
+            break
+        if filename.endswith('.json'):
+            file_path = os.path.join(directory_path, filename)
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                all_logs.extend(data['deviceLogs'])
+        amount_of_records -= 1
+
+    # Подготовка данных
+    df = pd.DataFrame(all_logs)
+    return df
+
+def predict():
+    df = load_dataframe_device_input(amount_of_records=128)
+    return predict_by_device_input(df)
+
+if __name__ == "__main__":
+    ret = predict()
+    json.dumps(ret)
--- process.py
+++ process.py
@@ -1,11 +1,22 @@
 import process_names.predict_mode_by_processes as predict_process
+import device_input.predict_mode_by_device_input as predict_device_input
 import user_session_manipulator.lock_session as lock_session
 
+def calculate_true_proportion(lst):
+    # Подсчитываем количество True в списке
+    true_count = sum(lst)
+    # Вычисляем общее количество элементов в списке
+    total_count = len(lst)
+    # Возвращаем долю True
+    return true_count / total_count if total_count > 0 else 0
+
 def main():
-    ret = predict_process.predict()
-    if ret[0][0] > 0.5:
-        lock_session.lock_windows()
-    print (ret)
+    print('predict_device_input.predict()')
+    print(predict_device_input.predict())
+    process_names_prediction = predict_process.predict()[0][0]
+    device_input_prediction = calculate_true_proportion(predict_device_input.predict())
+    ret = process_names_prediction / 2 + device_input_prediction / 2
+    return ret
 
 if __name__ == "__main__":
     main()
