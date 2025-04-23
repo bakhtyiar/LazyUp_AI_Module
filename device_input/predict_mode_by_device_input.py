7a491121001364f8718e883691e7701ff83c5dee
BakhtiyarKh <->
2025-04-23 18:58:05 +0300

    Update predict_mode_by_device_input.py

--- device_input/predict_mode_by_device_input.py
+++ device_input/predict_mode_by_device_input.py
@@ -1,54 +1,47 @@
-import json
 import os
-import pandas as pd
 import joblib
+import numpy as np
+from device_input.device_log_loader import load_device_logs
+from device_input.train_model_mode_by_device_input import prepare_dataset
 
 module_dir = os.path.dirname(os.path.abspath(__file__))
 
 directory_path = module_dir + './device_input_logs'  # Путь к директории с JSON-файлами
 model_path = module_dir + './predict_device_input.h5'  # Путь к модели
 
-def predict_by_device_input(df: pd.DataFrame):
-    # Преобразуем timestamp в числовой формат
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df['timestamp_seconds'] = df['timestamp'].astype('int64') // 10**9  # Конвертация в секунды
-    # Сортировка по timestamp
-    df = df.sort_values('timestamp')
+model = joblib.load(model_path)
 
-    # Выбираем необходимые столбцы
-    df = df[['timestamp_seconds', 'buttonKey', 'isWorkingMode']]
-    prepared_data = df
+def predict(sample_data):
+    """
+    Args:
+        список словарей с данными из каждого файла:
+        [
+            {
+                "mode": 0 | 1,
+                "list": [
+                    {"buttonKey": int, "dateTime": int (timestamp в мс)},
+                    ...
+                ]
+            },
+            ...
+        ]
 
-    model = joblib.load(model_path)
-
-    X = prepared_data[['timestamp_seconds', 'buttonKey']]
-    y = prepared_data['isWorkingMode']
-
-    # Оценка модели
-    ret = model.predict(X)
-    return ret
-
-def load_dataframe_device_input(amount_of_records=128):
-    # Считываем все логи
-    all_logs = []
-    for filename in os.listdir(directory_path):
-        if amount_of_records < 1:
-            break
-        if filename.endswith('.json'):
-            file_path = os.path.join(directory_path, filename)
-            with open(file_path, 'r') as file:
-                data = json.load(file)
-                all_logs.extend(data['deviceLogs'])
-        amount_of_records -= 1
+    Returns:
+        ndarray с предсказаниями
+        Array<0|1>
+    """
 
     # Подготовка данных
-    df = pd.DataFrame(all_logs)
-    return df
+    X, y = prepare_dataset(sample_data)
 
-def predict():
-    df = load_dataframe_device_input(amount_of_records=128)
-    return predict_by_device_input(df)
+    y_pred = model.predict(X)
+    return y_pred
+
+def save_y_pred_to_file(filename: str, arr: np.ndarray):
+    with open(filename or "y_pred.txt", 'a') as f:
+        np.savetxt(f, arr, fmt='%.4f')
 
 if __name__ == "__main__":
-    ret = predict()
-    json.dumps(ret)
+    ret = predict(sample_data=load_device_logs(1000))
+    print(ret)
+    save_y_pred_to_file("y_pred.txt", ret)
