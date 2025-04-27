d48e3cea03c95bb9d51b3b80d16e4eda6502a3f5
BakhtiyarKh <->
2025-04-27 14:36:12 +0300

    Removed docker related files due to system and security restrictions for app features

--- docker_stop.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Stop the container
-docker stop lazyup-ai-container
-
-REM Remove the container
-docker rm lazyup-ai-container
--- docker_stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Stop the container
-docker stop lazyup-ai-container
-
-# Remove the container
-docker rm lazyup-ai-container
